f8c4c53827c413bdb5e3ed1d980e100c55c9ab3e
Gökhan TIKNAZOGLU <gtiknazoglu@ulugenerji.com.tr>
2023-07-25 11:30:30 +0300

    ...

--- src/components/ChannelProgress.js
+++ src/components/ChannelProgress.js
@@ -1,6 +1,7 @@
 import {StyleSheet, Text, View} from "react-native";
 import {useEffect, useState} from "react";
 import MotionSlider from 'react-native-motion-slider';
+import {Slider} from "@rneui/base";
 
 
 const ChannelProgress = (props) => {
@@ -19,22 +20,33 @@ const ChannelProgress = (props) => {
     return (
         <View style={styles.container}>
             <View style={styles.item}>
-                <Text style={styles.text}>
-                    {props.ChannelName}: {value} %
-                </Text>
+                <View style={{flex: 2}}>
+                    <Text style={styles.text}>
+                        {props.ChannelName}: {value} %
+                    </Text>
+                </View>
 
-                <MotionSlider
-                    min={0}
-                    max={100}
-                    value={value}
-                    height={50}
-                    width={250}
-                    borderRadius={5}
-                    units={'%'}
-                    backgroundColor={['rgb(253,192,41)', 'rgb(223,135,29)', 'rgb(170,61,1)']}
-                    onValueChanged={(x) => setValue(x)}
-                    onPressOut={() => setReturnObj({channel: props.Channel, value: value})}
-                />
+                <View style={{flex: 3}}>
+
+                    <Slider
+                        style={{width: 170}}
+                        minimumTrackTintColor={'rgb(253,192,41)'}
+                        maximumTrackTintColor={'rgb(170,61,1)'}
+                        thumbTouchSize={{width: 40, height: 40}}
+                        thumbStyle={{width: 30, height: 30, backgroundColor: 'rgb(223,135,29)'}}
+                        thumbTintColor={'rgb(223,135,29)'}
+
+                        trackStyle={styles.track}
+                        //thumbStyle={styles.thumb}
+                        // minimumTrackTintColor='#e6a954'
+
+                        step={1}
+                        value={value}
+                        minimumValue={0}
+                        maximumValue={100}
+                        onValueChange={(value) => setValue(value)} onSlidingComplete={() => setReturnObj({channel: props.Channel, value: value})}/>
+                </View>
+
             </View>
         </View>
 
@@ -44,6 +56,7 @@ export default ChannelProgress;
 const styles = StyleSheet.create({
                                      container: {
                                          padding: 0,
+                                         flex:1
                                      },
                                      item: {
                                          display: "flex",
@@ -63,5 +76,22 @@ const styles = StyleSheet.create({
                                          marginRight: 10,
                                          alignItems: "stretch",
                                          justifyContent: "center"
+                                     },
+
+                                     track: {
+                                         height: 14,
+                                         borderRadius: 1,
+                                         backgroundColor: 'white',
+                                         borderColor: '#9a9a9a',
+                                         borderWidth: 1,
+                                     },
+                                     thumb: {
+                                         width: 20,
+                                         height: 20,
+                                         borderRadius: 2,
+                                         backgroundColor: '#eaeaea',
+                                         borderColor: '#9a9a9a',
+                                         borderWidth: 1,
                                      }
+
                                  });
